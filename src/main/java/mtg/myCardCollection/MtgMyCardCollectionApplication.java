ff42f7e26c61768ee8f026a61314b18959279163
ailedt <deliatreichler@gmail.com>
2016-06-23 12:16:54 +0200

    introduce eureka

--- src/main/java/mtg/myCardCollection/MtgMyCardCollectionApplication.java
+++ src/main/java/mtg/myCardCollection/MtgMyCardCollectionApplication.java
@@ -1,14 +1,19 @@
 package mtg.myCardCollection;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan
+@EnableEurekaClient
 public class MtgMyCardCollectionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MtgMyCardCollectionApplication.class, args);
+		SpringApplication notificationMicroService = new SpringApplication(MtgMyCardCollectionApplication.class);
+		notificationMicroService.addListeners(new ApplicationPidFileWriter("notification-micro-service.pid"));
+		notificationMicroService.run(args);
 	}
 }
